41712394fea7164b1f87038f3d5364d95af81892
Camilo Martin <fabian.martin@avaldigitallabs.com>
2024-11-13 20:25:05 -0500

    :rocket Aplicación inicializada

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:modelos_predictivos_agro/screens/home_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const HomeScreen());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Modelos Predictivos',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
