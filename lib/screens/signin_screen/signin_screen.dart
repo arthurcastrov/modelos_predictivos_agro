b863d04bdbefc27d1a6c286ef41a17838fd7eaaf
Camilo Martin <fabian.martin@avaldigitallabs.com>
2024-11-14 20:43:04 -0500

    :sparkles: Se agrega logica de go_router y autenticación con firebase

--- lib/screens/signin_screen/signin_screen.dart
+++ lib/screens/signin_screen/signin_screen.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:go_router/go_router.dart';
 import 'package:modelos_predictivos_agro/services/authentication.dart';
 
 class SigninScreen extends StatefulWidget {
